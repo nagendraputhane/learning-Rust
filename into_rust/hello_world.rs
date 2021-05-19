75d6e96c9c1710e6d89066c3469f64a87801bbd2
Nagendra Puthane <32806577+nagendraputhane@users.noreply.github.com>
2021-05-19 12:01:22 +0530

    remove goals

--- into_rust/hello_world.rs
+++ into_rust/hello_world.rs
@@ -1,8 +1,3 @@
-// Goals:
-// - change to greet you by name
-// - introduce a variable `name` and insert this into the format string
-// - try `println!("{}", name)` vs `println!("{:?}", name)` and see what the difference is
-
 fn main() {
     let lang = "Rust";
     println!("Hello, {:?}", lang);
