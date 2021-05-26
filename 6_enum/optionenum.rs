97c546e524d1b20af427a1774bdfab865a495131
Nagendra Puthane <32806577+nagendraputhane@users.noreply.github.com>
2021-05-26 09:21:49 +0530

    match for option

--- 6_enum/optionenum.rs
+++ 6_enum/optionenum.rs
@@ -1,7 +1,16 @@
 fn main() {
-    let some_number = Some(5);
-    let some_string = Some(" a string");
-
     let absent_number: Option<i32> = None; //currently not known, but will be known with type as Option<i32>
-    println!("{:?}", some_number);
+    println!("{:?}", absent_number);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(absent_number);
+    println!("six => {:?}, none => {:?}", six, none);
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
